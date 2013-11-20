8976711a326d8e2ea28c2df95fb050714d4deea5
druidvav <druidvav@gmail.com>
2013-11-20 15:09:29 +0400

    update

--- find-viruses.sh
+++ find-viruses.sh
@@ -7,18 +7,13 @@ fi
 echo "Searching in php files [001]..."
 find $1 -type f -name *.php | xargs -d "\n" -n1 -P8 grep -liFf "./db/001.txt"
 
-echo "Preparing filelist..."
-rm /tmp/av-php.tmp
-find $1 -type f -name *.php > /tmp/av-php.tmp
-echo "Searching in php files [6]..."
-xargs -d "\n" -n1 -P8 grep -li 'lzw_decompress(' < /tmp/av-php.tmp
-echo "Searching in php files [7]..."
-xargs -d "\n" -n1 -P8 grep -lie 'TrustlinkClient\|SAPE_base\|SAPE_client' < /tmp/av-php.tmp
-echo "Searching in php files [10]..."
-xargs -d "\n" -n1 -P8 grep -lie "base64_decode([\"']" < /tmp/av-php.tmp
-rm /tmp/av-php.tmp
+echo "Searching possibilities in php files [900]..."
+find $1 -type f -name *.php | xargs -d "\n" -n1 -P8 grep -liFf "./db/101.txt"
+
+echo "Searching in js files [201]..."
+find $1 -type f -name *.js | xargs -d "\n" -n1 -P8 grep -liFf "./db/201.txt"
 
 echo "Searching in htaccess..."
-find $1 -type f -name .htaccess -exec grep -li "RewriteCond %{HTTP_USER_AGENT} !" {} \;
+find $1 -type f -name .htaccess | xargs -d "\n" -n1 -P8 grep -li "RewriteCond %{HTTP_USER_AGENT} !"
 
 echo "Finished!"
