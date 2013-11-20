a761d46a41f628d44fbfa63470208d48dc3016e1
druidvav <druidvav@gmail.com>
2013-11-20 15:22:51 +0400

    update

--- find-viruses.sh
+++ find-viruses.sh
@@ -10,8 +10,11 @@ find $1 -type f -name *.php | xargs -d "\n" -n1 -P8 grep -liFf "./db/001.txt"
 echo "Searching possibilities in php files [900]..."
 find $1 -type f -name *.php | xargs -d "\n" -n1 -P8 grep -liFf "./db/101.txt"
 
-echo "Searching in js files [201]..."
-find $1 -type f -name *.js | xargs -d "\n" -n1 -P8 grep -liFf "./db/201.txt"
+echo "Searching for bad js files..."
+find $1 -type f -name *.js | xargs -d "\n" -n1 -P8 grep -li "^<?php"
+
+#echo "Searching in js files [201]..."
+#find $1 -type f -name *.js | xargs -d "\n" -n1 -P8 grep -liFf "./db/201.txt"
 
 echo "Searching in htaccess..."
 find $1 -type f -name .htaccess | xargs -d "\n" -n1 -P8 grep -li "RewriteCond %{HTTP_USER_AGENT} !"
